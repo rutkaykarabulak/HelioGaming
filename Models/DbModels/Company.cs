b4a50a5e000bc7e27961e9381e77b710920dcf45
rutkaykarabulak <rutkaykarabulak@gmail.com>
2023-02-05 23:43:02 +0100

    Add a property to show employee count in a company, update the database

---
+++ Models/DbModels/Company.cs
@@ -0,0 +1,50 @@
+﻿using HelioGaming.Models.EntityModels;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using HelioGaming.Utils;
+
+namespace HelioGaming.Models.DbModels
+{
+	/// <summary>
+	/// Representation of company object in database 
+	/// </summary>
+	public class Company
+	{
+		/// <summary>
+		/// Id of the company
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Name of the company
+		/// </summary>
+		[Required]
+		[RegularExpression(Constants.regexOnlyLettersNumbersAndDashes, ErrorMessage = Constants.regexOnlyLettersNumbersAndDashesErr)]
+		public string Name { get; set; } = string.Empty;
+		/// <summary>
+		/// Registration date of company
+		/// </summary>
+		
+		[Required]
+		public DateTime DateOfRegistration { get; set; }
+
+		/// <summary>
+		/// Address of the company
+		/// </summary>
+		public virtual Address Address { get; set; }
+
+		/// <summary>
+		/// Number of company
+		/// </summary>
+		public string Number { get; set; } = string.Empty;
+		/// <summary>
+		/// Phone number of company
+		/// </summary>
+		public string PhoneNumber { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Number of person who works in this company
+		/// </summary>
+		public int EmployeeCount { get; set; } = 0;
+	}
+}
