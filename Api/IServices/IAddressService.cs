60ed59ff209584d7e842a693963dabf786293691
rutkaykarabulak <rutkaykarabulak@gmail.com>
2023-02-06 21:02:22 +0100

    Creating services for entities

---
+++ Api/IServices/IAddressService.cs
@@ -0,0 +1,33 @@
+﻿using HelioGaming.Models;
+using HelioGaming.Models.DbModels;
+
+namespace HelioGaming.Api.IServices
+{
+	/// <summary>
+	/// Interface for address service that handles CRUD operations on address object
+	/// </summary>
+	public interface IAddressService
+	{
+		/// <summary>
+		/// Searchs for address entity with a given id in database
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>Address entity if there is a record were found, null otherwise</returns>
+		public abstract Task<AddressEntity>? Get(int id);
+		/// <summary>
+		/// Creates a address entity and insers into database
+		/// </summary>
+		/// <param name="address"></param>
+		/// <returns>Address entity that were created</returns>
+		public abstract Task<AddressEntity> Create(AddressEntity address);
+
+		/// <summary>
+		/// Removes the address entity with given id from database
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>True if removal is successfull, false otherwise</returns>
+		public abstract Task<bool> Remove(int id);
+
+		//public abstract Task<AddressEntity> Update(AddressEntity address); TODO
+	}
+}
---
+++ Api/IServices/ICompanyService.cs
@@ -0,0 +1,40 @@
+﻿using HelioGaming.Models;
+using HelioGaming.Models.DbModels;
+
+namespace HelioGaming.Api.IServices
+{
+	/// <summary>
+	/// Interface for company service that handes CRUD operations on company object.
+	/// </summary>
+	public interface ICompanyService
+	{
+		/// <summary>
+		/// Searchs for a given id in database.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>Company Entity if there is a record with a given id, null otherwise.</returns>
+		public abstract Task<CompanyEntity> Get(int id);
+
+		/// <summary>
+		/// Gets all the company record in database
+		/// </summary>
+		/// <returns>Enumerable objects of company entities</returns>
+		public abstract IEnumerable<CompanyEntity> GetAll();
+
+		/// <summary>
+		/// Creates a company object and inserts into database
+		/// </summary>
+		/// <param name="company"></param>
+		/// <returns>Objects that were created</returns>
+		public abstract Task<CompanyEntity> Create(CompanyEntity company);
+
+		//public abstract Task<CompanyEntity> Update();  TODO with put object
+
+		/// <summary>
+		/// Removes the object with given id.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>True, if removal is successfull, false otherwise</returns>
+		public abstract Task<bool> Remove(int id);
+	}
+}
